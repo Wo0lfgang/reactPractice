5128d3a7c1f643defa20e384855cca818889eb26
Wo0lfgang <2356269138@qq.com>
2022-09-15 11:52:58 +0800

    React框架基础部分

---
+++ react_staging/02_src_hello_react/App.js
@@ -0,0 +1,20 @@
+// 创建“外壳”组件App
+// 引入React
+import React,{Component} from "react";
+
+import Hello from "./components/Hello";
+import Welcome from "./components/Welcome";
+
+// 创建并暴露App组件
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                <Hello/>
+                <Welcome/>
+            </div>
+        )
+    }
+}
+
+
---
+++ react_staging/02_src_hello_react/components/Hello/index.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import hello from './index.module.css'
+export default class Hello extends Component{
+    render(){
+        return(
+            <div className={hello.title}>
+                Hello,React!!!
+            </div>
+        )
+    }
+}
