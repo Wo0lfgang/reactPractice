64857da63287993476d995342bd2669ef8a97f35
大白 <2356269138@qq.com>
2022-09-20 23:02:50 +0800

    部分路由知识

--- react_staging/10_src_封装NavLink/App.js
+++ react_staging/10_src_封装NavLink/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import MyNavLink from './components/MyNavLink'
-import Test from './pages/Test'
 import './App.css'
 
 export default class App extends Component {
@@ -16,27 +15,24 @@ export default class App extends Component {
                         <div class="page-header"><h2>React Router Demo</h2></div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-xs-2 col-xs-offset-2">
-                        <div class="list-group">
-                            {/* <a class="list-group-item" href="./about.html">About</a>
+                    <div class="row">
+                        <div class="col-xs-2 col-xs-offset-2">
+                            <div class="list-group">
+                                {/* <a class="list-group-item" href="./about.html">About</a>
                             <a class="list-group-item active" href="./home.html">Home</a> */}
-                            <MyNavLink to="/about" title="about" children="about" />
-                            <MyNavLink to="/home" title="home" children="home" />
+                                <MyNavLink to="/about" title="about" children="about"/>
+                                <MyNavLink to="/home" title="home" children="home"/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-xs-6">
-                        <div class="panel">
-                            <div class="panel-body">
-                                <Switch>
+                        <div class="col-xs-6">
+                            <div class="panel">
+                                <div class="panel-body">
                                     <Route path="/about" component={About} />
                                     <Route path="/home" component={Home} />
-                                    <Route path="/home" component={Test} />
-                                </Switch>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
             </div>
         )
     }
